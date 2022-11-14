08908bd522f531e9dceb708c7845bdb09a0d70fa
ztonege <nianhsuan@gmail.com>
2022-11-14 00:59:29 +0000

    add tartanvo

---
+++ TartanVO/install_torch.sh
@@ -0,0 +1,3 @@
+# Install torch with this command because we are using cuda 11.4
+# https://discuss.pytorch.org/t/nvidia-a100-gpu-runtimeerror-cudnn-error-cudnn-status-mapping-error/121648/2
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
